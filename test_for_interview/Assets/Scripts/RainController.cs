018fa2d91298c154274088e553463b7c5044d13b
Ivan <1996altair1996@gmail.com>
2018-08-10 01:17:44 +0300

    added rain and wind controllers, plane was changed by terrain

---
+++ test_for_interview/Assets/Scripts/RainController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RainController : MonoBehaviour {
+
+    [SerializeField] private GameObject m_Clouds;
+    [SerializeField] private GameObject m_Raining;
+    [SerializeField] private Light m_Light;
+
+    private float m_ShadowStrengthForRain;
+    private Color32 m_LightColorForRain;
+
+    private void Awake()
+    {
+         m_ShadowStrengthForRain = 0.15f;
+         m_LightColorForRain = new Color32(175, 175, 175, 255);
+    }
+
+    public void rainign(bool enable)
+    {
+        m_Clouds.SetActive(enable);
+        m_Raining.SetActive(enable);
+        if(enable)
+        {
+            m_Light.shadowStrength = m_ShadowStrengthForRain;
+            m_Light.color = m_LightColorForRain;
+        }
+    }
+
+
+}
---
+++ test_for_interview/Assets/Scripts/WeatherController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherController : MonoBehaviour {
+
+    [SerializeField] private ParticleSystem m_Snowing;
+    [SerializeField] private GameObject m_Wind;
+    public bool Wind;
+    [SerializeField] private RainController m_RainController;
+    public bool Rain;
+    // Use this for initialization
+    void Start () {
+        if(m_RainController)
+          m_RainController.rainign(Rain);
+        m_Wind.SetActive(Wind);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
