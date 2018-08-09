9c2c6da851bc945c487073965dd2d3becad1bac9
Ivan <1996altair1996@gmail.com>
2018-08-09 14:41:18 +0300

    added houses, rain, snow

--- test_for_interview/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
+++ test_for_interview/Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/AICharacterControl.cs
@@ -17,9 +17,10 @@ namespace UnityStandardAssets.Characters.ThirdPerson
             // get the components on the object we need ( should not be null due to require component so no need to check )
             agent = GetComponentInChildren<UnityEngine.AI.NavMeshAgent>();
             character = GetComponent<ThirdPersonCharacter>();
-
+            
 	        agent.updateRotation = false;
 	        agent.updatePosition = true;
+            agent.Warp(new Vector3(0, 1, 0));
         }
 
 
